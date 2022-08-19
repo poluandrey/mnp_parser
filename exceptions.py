749cad168c743c385ac212016ed0292ad2188c66
a_polumestny <anpolumestny@gmail.com>
2022-08-19 17:50:19 +0300

    add new exceptions

--- exceptions.py
+++ exceptions.py
@@ -4,4 +4,15 @@ class Error(Exception):
 
 
 class ConfigLoadError(Error):
+    """raise in case error during load config"""
+    pass
+
+
+class CreateConfigDirError(Error):
+    """raise in case of error during creating not existing directory specified in settings"""
+    pass
+
+
+class SourceMnpFileNotExists(Error):
+    """raise in case of source mnp file not exists"""
     pass
